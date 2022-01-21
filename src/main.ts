24129682a57b0be9bc5f65004c8a727505f99a6c
sergej.volobuev <serjant.saygon@gmail.com>
2022-01-21 15:35:42 +0000

    commit

--- src/main.ts
+++ src/main.ts
@@ -24,14 +24,17 @@ async function main() {
     app.listen(configService.port);
 
     Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next);
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
-
-            } else if (result !== null && result !== undefined) {
-                res.json(result);
-            }
+        (app as any)[route.method]
+            (route.route, (req: Request, res: Response, next: Function) => {
+                const result = (new (route.controller as any))[route.action](req, res, next);
+                if (result instanceof Promise) {
+                        result.then(result => result !== null &&
+                        result !== undefined ? res.send(result) : 
+                        undefined
+                    );
+                } else if (result !== null && result !== undefined) {
+                    res.json(result);
+                }
         });
     });
 }
